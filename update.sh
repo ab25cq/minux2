af74dc62892e074a5cfd67eca27bce4457900dfc
ab25cq <ab25cq@icloud.com>
2025-01-09 15:23:24 +0900

    first commit

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export EDITOR=vin
+git config --global user.email ab25cq@icloud.com
+git config --global user.name ab25cq
+
+git add .
+git commit 
+git remote add origin git@github.com:ab25cq/minux2.git
+git push -f -u origin main
+
