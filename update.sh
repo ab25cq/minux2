a7bb12f10d5baf2345dd652e254dc5cb70cb6355
ab25cq <ab25cq@icloud.com>
2025-01-09 15:25:30 +0900

    first commit

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ git config --global user.email ab25cq@icloud.com
 git config --global user.name ab25cq
 
 git add .
-git commit 
-git remote set-url origin git@github.com:ab25cq/minux2.git
-git push -f -u origin master
-
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ab25cq/minux2.git
+git push -u origin main
